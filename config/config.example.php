4fd395fe5171458de5bf5e513453072fda89b5cf
Pete Lee <welikeheon@gmail.com>
2020-06-30 22:18:33 +0900

    민감한 정보 삭제

--- config/config.example.php
+++ config/config.example.php
@@ -1,11 +1,14 @@
 <?php /** @noinspection SyntaxError */
-$host=
-$user=
-$password=
-$database=
+$host="";
+$user="";
+$password="";
+$database="";
 
-$AWS_ACCESS_KEY_ID=
-$AWS_SECRET=
-$AWS_DEFAULT_REGION=
-$AWS_BUCKET=
+$AWS_ACCESS_KEY_ID="";
+$AWS_SECRET="";
+$AWS_DEFAULT_REGION="";
+$AWS_BUCKET="";
+
+$NAVER_CLIENT_ID="";
+$NAVER_CLIENT_SECRET="";
 ?>
